aa44701130b5d6d1276b6561c00ccc78c76be6b5
Timothy Cope <kirbycope@gmail.com>
2025-03-09 16:47:02 -0700

    Follow and Jiggle

---
+++ scenes/main/main.gd
@@ -0,0 +1,29 @@
+extends Node3D
+
+var angle := 0.0
+@export var orbit_radius: float = 0.4
+@export var orbit_speed: float = 1.0
+@onready var look_at: LookAtModifier3D = $Shuna/Armature/Skeleton3D/LookAtModifier3D
+@onready var sphere: MeshInstance3D = $Sphere
+
+
+func _ready() -> void:
+	look_at.target_node = sphere.get_path()
+
+func _process(delta):
+
+	# Update the angle
+	angle -= orbit_speed * delta
+
+	# Calculate new position
+	var x = orbit_radius * cos(angle)
+	var z = orbit_radius * sin(angle)
+
+	# Update the sphere position
+	sphere.position = Vector3(x, sphere.position.y, z)
+	if angle < -3:
+		look_at.target_node = sphere.get_path()
+	else:
+		look_at.target_node = ""
+	if angle < -6.0:
+		angle = 0.0
