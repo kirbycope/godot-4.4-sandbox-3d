fdccf83e9f4615a1263ec1e596a8cea719c39f00
Timothy Cope <kirbycope@gmail.com>
2025-03-09 20:55:50 -0700

    rimuru

--- credits.md
+++ credits.md
@@ -1,3 +1,4 @@
 # Credits
 
 [Shuna](https://www.models-resource.com/mobile/thattimeigotreincarnatedasaslimeisekaimemories/model/74342/) - Eeveeloverthespriter8
+[Rimuru](https://sketchfab.com/3d-models/rimuru-slime-612ff2c805114744b66d3c29c7942371) - tommdraws
--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -3,9 +3,9 @@ extends Node3D
 var angle := 0.0
 @export var orbit_radius: float = 0.4
 @export var orbit_speed: float = 1.0
-@onready var look_at: LookAtModifier3D = $Shuna/Armature/Skeleton3D/LookAtModifier3D
-@onready var sphere: MeshInstance3D = $Sphere
-
+@onready var shuna: Node3D = $Shuna
+@onready var shuna_look_at: LookAtModifier3D = $Shuna/Armature/Skeleton3D/LookAtModifier3D
+@onready var rimuru: Node3D = $Rimuru
 
 func _process(delta):
 
@@ -16,11 +16,16 @@ func _process(delta):
 	var x = orbit_radius * cos(angle)
 	var z = orbit_radius * sin(angle)
 
-	# Update the sphere position
-	sphere.position = Vector3(x, sphere.position.y, z)
+	# Update Rimuru's position
+	rimuru.position = Vector3(x, rimuru.position.y, z)
 	if angle < -3:
-		look_at.target_node = sphere.get_path()
+		shuna_look_at.target_node = rimuru.get_path()
 	else:
-		look_at.target_node = ""
+		shuna_look_at.target_node = ""
 	if angle < -6.0:
 		angle = 0.0
+
+	# Make Rimuru face Shuna
+	var shuna_pos = shuna.global_position
+	rimuru.look_at(Vector3(0, 1.3, 0))
+	rimuru.rotate_y(-1.5708)
